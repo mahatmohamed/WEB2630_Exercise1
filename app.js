a434b014542fa58ece0005c715c3c8f21771a49a
Mahat Mohamed <Mahatmohamed@mail.weber.edu>
2021-09-21 01:00:25 -0500

    new changes

---
+++ app.js
@@ -0,0 +1,31 @@
+new Vue({
+	el: '#exercise',
+	data: {
+		name: 'Mahat',
+		age: 27,
+		image: '',
+    title: 'dynamicText',
+
+    food: [
+      { fruits: 'tomato' },
+      { fruits: 'banana' },
+      { fruits: 'lemon' }
+    ]
+  },
+
+	
+	methods: {
+		ageMultiplied: function(multiplier){
+			return this.age * multiplier
+		},
+    dynamicText(event) {
+      this.title = event.target.value;
+    },
+		randomNum: function(){
+			return Math.random()
+		},
+    alertMe () {
+      alert('Alert!')
+    }
+	}
+})
